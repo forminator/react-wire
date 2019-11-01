7f71d0300aa61f165ace4ca167878d5aef47f395
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2019-11-01 06:22:11 +0330

    docs: add and build api docs

--- src/use-wire-value.ts
+++ src/use-wire-value.ts
@@ -1,9 +1,31 @@
 import { useDebugValue, useEffect, useState } from 'react';
 import { Subscribable } from './subscribable';
 
+/**
+ *
+ * returns wire value and subscribe to wire for value updates
+ *
+ * @param wire
+ *
+ * @remarks
+ * please always pass same wire param and avoid changing wire param, it can cause strange behavior and bad intermediate values
+ *
+ */
 export function useWireValue<Value>(
   wire: Subscribable<Value> | null | undefined,
 ): Value | undefined;
+/**
+ *
+ * returns wire value and subscribe to wire for value updates
+ *
+ * @param wire
+ * @param defaultValue - return value if wire value is undefined
+ *
+ * @remarks
+ *
+ * please always pass same wire param and avoid changing wire param, it can cause strange behavior and bad intermediate values
+ *
+ */
 export function useWireValue<Value>(
   wire: Subscribable<Value> | null | undefined,
   defaultValue: Value,
--- src/use-wire.ts
+++ src/use-wire.ts
@@ -3,6 +3,28 @@ import { isInitializer } from './is-initializer';
 import { Wire } from './wire';
 import { _WireImpl } from './wire.impl';
 
+/**
+ *
+ *
+ * creates and returns a new wire. 
+ * 
+ * @typeParam Value - the type of the value of wire
+ * 
+ * @param upLink - the up-link wire to be connected to and sync value with
+ * @param initialValue - initial value or initializer function
+ * @returns new wire
+ * 
+ * @remarks
+ * if up-link wire param provided, the newly created wire will be connected to up-link and they have the same value
+ * 
+ * if `upLink` param provided and up-link wire has a value, this wire respect up-link value and ignore its own initial value.
+ * 
+ * if `upLink` param provided and up-link wire hasn't a value (has `undefined` value) and this wire has a value, up-link value will be updated
+ * 
+ * please always pass same wire as up-link and avoid changing up-link wire, it can cause strange behavior and bad intermediate values
+
+ *
+ */
 export function useWire<Value>(
   upLink: Wire<Value> | null | undefined,
   initialValue?: Value | (() => Value),
