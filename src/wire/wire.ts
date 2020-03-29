104fb97475f5e3623a40b45387b2db28fc3e037d
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2020-03-30 01:22:28 +0430

    fix: add default Fns type to wire

--- src/wire/wire.ts
+++ src/wire/wire.ts
@@ -1,4 +1,4 @@
 import { FnsWire } from '../fn-wire/fns-wire';
 import { StateWire } from '../state-wire/state-wire';
 
-export type Wire<V, Fns> = StateWire<V> & FnsWire<Fns>;
+export type Wire<V, Fns = {}> = StateWire<V> & FnsWire<Fns>;
