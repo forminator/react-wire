5c7bd97d19df26e10da02d3c0b13b6d8d5789559
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2020-06-17 16:08:35 +0430

    feat(state-selector): add createStateSelector

---
+++ src/state-selector/create-state-selector.test.ts
@@ -0,0 +1,251 @@
+import { createStateWire } from '../state-wire/create-state-wire';
+import { createStateSelector } from './create-state-selector';
+
+const createDoubleValueSelector = () => {
+  const [wire, connectWire] = createStateWire({}, 2);
+  connectWire();
+  const [selector, connect] = createStateSelector({
+    get: ({ get }) => {
+      return get(wire) * 2;
+    },
+  });
+  return { selector, connect, wire };
+};
+
+const createAreaValueSelector = () => {
+  const [wire1, connectWire1] = createStateWire({}, 2);
+  const [wire2, connectWire2] = createStateWire({}, 3);
+  connectWire1();
+  connectWire2();
+  const [selector, connect] = createStateSelector({
+    get: ({ get }) => {
+      return get(wire1) * get(wire2);
+    },
+  });
+  return { selector, connect, wire1, wire2 };
+};
+
+const createConditionalSelector = () => {
+  const [wire1, connectWire1] = createStateWire({}, 2);
+  const [wire2, connectWire2] = createStateWire({}, 3);
+  const [wire3, connectWire3] = createStateWire<boolean>({}, true);
+  connectWire1();
+  connectWire2();
+  connectWire3();
+  const [selector, connect] = createStateSelector({
+    get: ({ get }) => {
+      return get(wire3) ? get(wire1) : get(wire2);
+    },
+  });
+  return { selector, connect, wire1, wire2, wire3 };
+};
+
+const createWritableDoubleValueSelector = () => {
+  const [wire, connectWire] = createStateWire({}, 2);
+  connectWire();
+  const [selector, connect] = createStateSelector<number>({
+    get: ({ get }) => {
+      return get(wire) * 2;
+    },
+    set: ({ set }, value) => {
+      set(wire, value / 2);
+    },
+  });
+  return { selector, connect, wire };
+};
+
+const createWritableAreaValueSelector = (value1: number, value2: number) => {
+  const [wire1, connectWire1] = createStateWire({}, value1);
+  const [wire2, connectWire2] = createStateWire({}, value2);
+  connectWire1();
+  connectWire2();
+  const [selector, connect] = createStateSelector<number>({
+    get: ({ get }) => {
+      return get(wire1) * get(wire2);
+    },
+    set: ({ get, set }, value) => {
+      set(wire2, value / get(wire1));
+    },
+  });
+  return { selector, connect, wire1, wire2 };
+};
+
+describe('selector', () => {
+  describe('single wire', () => {
+    it('should have correct initial value before connect ', () => {
+      const { selector } = createDoubleValueSelector();
+      expect(selector.getValue()).toBe(4);
+    });
+    it('should have correct value after connect ', () => {
+      const [wire, connectWire] = createStateWire({}, 2);
+      connectWire();
+      const [selector, connect] = createStateSelector({
+        get: ({ get }) => {
+          return get(wire) * 2;
+        },
+      });
+      connect();
+      expect(selector.getValue()).toBe(4);
+    });
+    it('should update value after connect if wire value changed ', () => {
+      const { selector, wire, connect } = createDoubleValueSelector();
+      wire.setValue(3);
+      connect();
+      expect(selector.getValue()).toBe(6);
+    });
+    it('should update value after wire value changed ', () => {
+      const { selector, wire, connect } = createDoubleValueSelector();
+      connect();
+      wire.setValue(3);
+      expect(selector.getValue()).toBe(6);
+    });
+    it('should call subscribe function when value changed ', () => {
+      const { selector, wire, connect } = createDoubleValueSelector();
+      connect();
+      const fn = jest.fn();
+      const unsubscribe = selector.subscribe(fn);
+      wire.setValue(3);
+      unsubscribe();
+      expect(fn).toBeCalledWith(6);
+    });
+    it('should not call subscribe function after unsubscribe ', () => {
+      const { selector, wire, connect } = createDoubleValueSelector();
+      connect();
+      const fn = jest.fn();
+      const unsubscribe = selector.subscribe(fn);
+      unsubscribe();
+      wire.setValue(3);
+      expect(fn).not.toBeCalled();
+    });
+    it('should not change after disconnect ', () => {
+      const { selector, wire, connect } = createDoubleValueSelector();
+      const disconnect = connect();
+      const fn = jest.fn();
+      const unsubscribe = selector.subscribe(fn);
+      disconnect();
+      wire.setValue(3);
+      unsubscribe();
+      expect(selector.getValue()).toBe(4);
+      expect(fn).not.toBeCalled();
+    });
+  });
+  describe('multiple wires', () => {
+    it('should have correct initial value before connect ', () => {
+      const { selector } = createAreaValueSelector();
+      expect(selector.getValue()).toBe(6);
+    });
+    it('should have correct value after connect ', () => {
+      const { selector, connect } = createAreaValueSelector();
+      connect();
+      expect(selector.getValue()).toBe(6);
+    });
+    it('should update value after connect if wire value changed ', () => {
+      const { selector, connect, wire1 } = createAreaValueSelector();
+      wire1.setValue(3);
+      connect();
+      expect(selector.getValue()).toBe(9);
+    });
+    it('should update value after wire value changed ', () => {
+      const { selector, connect, wire1 } = createAreaValueSelector();
+      connect();
+      wire1.setValue(3);
+      expect(selector.getValue()).toBe(9);
+    });
+    it('should call subscribe function when value changed ', () => {
+      const { selector, connect, wire1 } = createAreaValueSelector();
+      connect();
+      const fn = jest.fn();
+      const unsubscribe = selector.subscribe(fn);
+      wire1.setValue(3);
+      unsubscribe();
+      expect(fn).toBeCalledWith(9);
+    });
+  });
+  describe('conditional selector', () => {
+    it('should have correct initial value before connect ', () => {
+      const { selector } = createConditionalSelector();
+      expect(selector.getValue()).toBe(2);
+    });
+    it('should have correct value after connect ', () => {
+      const { selector, connect } = createConditionalSelector();
+      connect();
+      expect(selector.getValue()).toBe(2);
+    });
+
+    describe('change value', () => {
+      it('should update value after connect if wire value changed ', () => {
+        const { selector, connect, wire1 } = createConditionalSelector();
+        wire1.setValue(4);
+        connect();
+        expect(selector.getValue()).toBe(4);
+      });
+      it('should update value after wire value changed ', () => {
+        const { selector, connect, wire1 } = createConditionalSelector();
+        connect();
+        wire1.setValue(4);
+        expect(selector.getValue()).toBe(4);
+      });
+      it('should call subscribe function when value changed ', () => {
+        const { selector, connect, wire1 } = createConditionalSelector();
+        connect();
+        const fn = jest.fn();
+        const unsubscribe = selector.subscribe(fn);
+        wire1.setValue(4);
+        unsubscribe();
+        expect(fn).toBeCalledWith(4);
+      });
+      it('should not call subscribe function when unused value changed ', () => {
+        const { selector, connect, wire2 } = createConditionalSelector();
+        connect();
+        const fn = jest.fn();
+        const unsubscribe = selector.subscribe(fn);
+        wire2.setValue(5);
+        unsubscribe();
+        expect(fn).not.toBeCalled();
+      });
+    });
+
+    describe('change condition', () => {
+      it('should update value after connect if wire value changed ', () => {
+        const { selector, connect, wire3 } = createConditionalSelector();
+        wire3.setValue(false);
+        connect();
+        expect(selector.getValue()).toBe(3);
+      });
+      it('should update value after wire value changed ', () => {
+        const { selector, connect, wire3 } = createConditionalSelector();
+        connect();
+        wire3.setValue(false);
+        expect(selector.getValue()).toBe(3);
+      });
+      it('should call subscribe function when value changed ', () => {
+        const { selector, connect, wire3 } = createConditionalSelector();
+        connect();
+        const fn = jest.fn();
+        const unsubscribe = selector.subscribe(fn);
+        wire3.setValue(false);
+        unsubscribe();
+        expect(fn).toBeCalledWith(3);
+      });
+    });
+  });
+  describe('writable selector', () => {
+    it('should update wire value ', () => {
+      const { selector, connect, wire } = createWritableDoubleValueSelector();
+      connect();
+      selector.setValue(8);
+      expect(wire.getValue()).toBe(4);
+      expect(selector.getValue()).toBe(8);
+    });
+    it('should update wire value ', () => {
+      const { selector, connect, wire2 } = createWritableAreaValueSelector(
+        2,
+        3,
+      );
+      connect();
+      selector.setValue(12);
+      expect(wire2.getValue()).toBe(6);
+      expect(selector.getValue()).toBe(12);
+    });
+  });
+});
---
+++ src/state-selector/create-state-selector.ts
@@ -0,0 +1,118 @@
+import mitt, { Emitter } from 'mitt';
+import {
+  createReadonlyStateWireGuard,
+  ReadonlyStateWire,
+} from '../state-wire/readonly-state-wire';
+import { createStateWireGuard } from '../state-wire/state-wire';
+import { StateWire } from '../types';
+import { Defined } from '../utils/type-utils';
+
+type DisconnectFunction = () => void;
+type ConnectFunction = () => DisconnectFunction;
+
+export type GetWireValue = <V>(wire: ReadonlyStateWire<V>) => V;
+export type SetWireValue = <V>(wire: StateWire<V>, value: Defined<V>) => void;
+
+export interface ReadOnlySelectorOptions<V> {
+  get: (options: { get: GetWireValue }) => V;
+}
+
+export interface WritableSelectorOptions<V> {
+  get: (options: { get: GetWireValue }) => V;
+  set: (options: { get: GetWireValue; set: SetWireValue }, value: V) => void;
+}
+
+export interface SelectorOptions<V> {
+  get: (options: { get: GetWireValue }) => V;
+  set?: (options: { get: GetWireValue; set: SetWireValue }, value: V) => void;
+}
+
+export function createStateSelector<V>(
+  options: WritableSelectorOptions<V>,
+): [StateWire<V>, ConnectFunction];
+export function createStateSelector<V>(
+  options: ReadOnlySelectorOptions<V>,
+): [ReadonlyStateWire<V>, ConnectFunction];
+export function createStateSelector<V>(
+  options: SelectorOptions<V>,
+): [ReadonlyStateWire<V> | StateWire<V>, ConnectFunction] {
+  const key: string = 'value';
+  const activeWires: {
+    current: Map<ReadonlyStateWire<any>, null | (() => void)>;
+  } = {
+    current: new Map(),
+  };
+  const { get: getOption, set: setOption } = options;
+  const get = <V>(wire: ReadonlyStateWire<V>) => {
+    activeWires.current.set(wire, null);
+    return wire.getValue();
+  };
+  const setGet = <V>(wire: ReadonlyStateWire<V>) => {
+    return wire.getValue();
+  };
+  const set = <V>(wire: StateWire<V>, value: Defined<V>) => {
+    wire.setValue(value);
+  };
+  let stateValue = getOption({ get });
+  const emitter: Emitter = mitt();
+
+  const update = () => {
+    const oldActiveWires = activeWires.current;
+    const oldValue = stateValue;
+    const newActiveWires = new Map();
+    activeWires.current = newActiveWires;
+    const newValue = getOption({ get });
+
+    oldActiveWires.forEach((unsubscribe, wire) => {
+      if (unsubscribe && !newActiveWires.has(wire)) {
+        unsubscribe();
+      }
+    });
+    newActiveWires.forEach((unsubscribe, wire) => {
+      const newUnsubscribe =
+        oldActiveWires.get(wire) ??
+        wire.subscribe(() => {
+          update();
+        });
+
+      newActiveWires.set(wire, newUnsubscribe);
+    });
+
+    if (oldValue !== newValue) {
+      stateValue = newValue;
+      emitter.emit(key, newValue);
+    }
+    return () => {
+      const oldActiveWires = activeWires.current;
+      activeWires.current = new Map();
+      oldActiveWires.forEach(unsubscribe => {
+        unsubscribe && unsubscribe();
+      });
+    };
+  };
+
+  const connect = () => {
+    return update();
+  };
+
+  const getValue = () => {
+    return stateValue;
+  };
+
+  const setValue = (value: Defined<V>) => {
+    setOption && setOption({ get: setGet, set }, value);
+  };
+
+  const subscribe = (callback: (value: Defined<V>) => void) => {
+    emitter.on(key, callback);
+    return () => {
+      emitter.off(key, callback);
+    };
+  };
+
+  const selectorContext: ReadonlyStateWire<V> | StateWire<V> = setOption
+    ? { getValue, subscribe, setValue, ...createStateWireGuard() }
+    : { getValue, subscribe, ...createReadonlyStateWireGuard() };
+
+  return [selectorContext, connect];
+}
