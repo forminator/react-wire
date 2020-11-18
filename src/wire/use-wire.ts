b51ea139b03bb4a9d09feae5a9bfff1ea0720735
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2020-11-18 20:01:16 +0330

    feat(wire): remove stability guard

--- src/wire/use-wire.ts
+++ src/wire/use-wire.ts
@@ -2,7 +2,6 @@ import { useMemo } from 'react';
 import { useFnsWire } from '../fn-wire/use-fns-wire';
 import { useStateWire } from '../state-wire/use-state-wire';
 import { InitializerOrValue } from '../utils/is-initializer';
-import { useStabilityGuard } from '../utils/use-stability-guard';
 import { Wire } from './wire';
 
 export function useWire<V, Fns = {}>(upLink: Wire<V, Fns>): Wire<V, Fns>;
@@ -22,7 +21,6 @@ export function useWire<V, Fns = {}>(
   upLink: Wire<V | undefined, Fns> | null | undefined,
   initialValue?: InitializerOrValue<V | undefined>,
 ): Wire<V | undefined, Fns> | Wire<V, Fns> {
-  useStabilityGuard(upLink);
   const fnsWire = useFnsWire(upLink);
   const stateWire = useStateWire(upLink, initialValue);
 
