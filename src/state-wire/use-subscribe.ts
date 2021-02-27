657b107b9a947afcc4c282f1ffb5cb65f9d9e6ad
Seyyed Morteza Moosavi <smmoosavi@users.noreply.github.com>
2021-02-27 19:51:48 +0330

    refactor: remove stability guard (#35)

--- src/state-wire/use-subscribe.ts
+++ src/state-wire/use-subscribe.ts
@@ -1,13 +1,11 @@
 import { useEffect } from 'react';
 import { Defined } from '../utils/type-utils';
-import { useStabilityGuard } from '../utils/use-stability-guard';
 import { ReadonlyStateWire } from './readonly-state-wire';
 
 export function useSubscribe<V>(
   wire: ReadonlyStateWire<V>,
   callback: (value: Defined<V>) => void,
 ): void {
-  useStabilityGuard(wire);
   useEffect(() => {
     return wire.subscribe(callback);
   }, [wire, callback]);
