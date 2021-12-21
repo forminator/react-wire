de15ac4b2ec14e8ddf35adbb0d56d2eaf5a74495
Seyyed Morteza Moosavi <smmoosavi@users.noreply.github.com>
2021-12-21 17:22:58 +0330

    feat(state-wire): use use-sync-external-store (#50)

--- src/state-wire/use-wire-state.ts
+++ src/state-wire/use-wire-state.ts
@@ -1,16 +1,10 @@
-import {
-  Dispatch,
-  SetStateAction,
-  useCallback,
-  useDebugValue,
-  useEffect,
-  useState,
-} from 'react';
+import { Dispatch, SetStateAction, useCallback, useDebugValue } from 'react';
 import { InitializerOrValue } from '../utils/is-initializer';
 import { isSetStateAction } from '../utils/is-set-state-action';
 import { Defined, isDefined } from '../utils/type-utils';
 import { StateWire } from './state-wire';
 import { useStateWire } from './use-state-wire';
+import { useWireValue } from './use-wire-value';
 
 type valueAndAction<V> = [V, Dispatch<SetStateAction<Defined<V>>>];
 
@@ -32,20 +26,8 @@ export function useWireState<V>(
   initialValue?: InitializerOrValue<V | undefined>,
 ): valueAndAction<V | undefined> | valueAndAction<V> {
   const innerWire = useStateWire(wire, initialValue);
-  const valueToReturn = innerWire.getValue();
-  const [stateValue, setStateValue] = useState(() => innerWire.getValue());
+  const valueToReturn = useWireValue(innerWire);
   useDebugValue(valueToReturn);
-  useEffect(() => {
-    const wireValue = innerWire.getValue();
-    if (isDefined(wireValue) && wireValue !== stateValue) {
-      setStateValue(wireValue);
-    }
-    return innerWire.subscribe((value) => {
-      setStateValue(value);
-    });
-    // `stateValue` variable only used as initial value
-    // eslint-disable-next-line react-app/react-hooks/exhaustive-deps
-  }, [innerWire]);
 
   const setValue = useCallback(
     (value: SetStateAction<Defined<V>>) => {
