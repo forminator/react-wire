c0261ae03b444e67c3464705751f29dda2e65025
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2022-01-29 13:05:52 +0330

    test: ensured use-state and use-wire-value is in sync
    related issue: https://github.com/facebookexperimental/Recoil/issues/1076

--- src/selector/use-selector.test.ts
+++ src/selector/use-selector.test.ts
@@ -1,4 +1,6 @@
-import { renderHook } from '@testing-library/react-hooks';
+import { act, renderHook } from '@testing-library/react-hooks';
+import { useEffect, useState } from 'react';
+import { useWireValue } from '../state-wire/use-wire-value';
 import { useWire } from '../wire/use-wire';
 import { useSelector } from './use-selector';
 
@@ -11,5 +13,21 @@ describe('use-selector', () => {
     });
 
     expect(result.current.selector.getValue()).toBe(6);
+  });
+  it('should updated in sync with react useState', function () {
+    const { result } = renderHook(() => {
+      const wire = useWire(null, 0);
+      const selector = useSelector({ get: ({ get }) => get(wire) * 2 });
+      const selectorValue = useWireValue(selector);
+      const [value, setValue] = useState(0);
+      useEffect(() => {
+        expect(selectorValue).toBe(value * 2);
+      });
+      return { wire, setValue };
+    });
+    act(() => {
+      result.current.setValue(1);
+      result.current.wire.setValue(1);
+    });
   });
 });
