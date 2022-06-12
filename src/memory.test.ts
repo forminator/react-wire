a81dd04278913644f3d4c854334bcd1b262d9892
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2022-06-12 16:59:41 +0430

    test: add memory leakage tests for fns wires

--- src/memory.test.ts
+++ src/memory.test.ts
@@ -1,4 +1,5 @@
 import LeakDetector from 'jest-leak-detector';
+import { createFnsWire } from './fn-wire/create-fns-wire';
 import { createStateSelector } from './state-selector/create-state-selector';
 import { createStateWire } from './state-wire/create-state-wire';
 
@@ -46,6 +47,54 @@ describe('memory', () => {
       expect(await detector.isLeaking()).toBe(true);
       selector = null;
       expect(await detector.isLeaking()).toBe(false);
+    });
+  });
+
+  describe('create fns wire', () => {
+    it('should not leak wire after subscribe', async () => {
+      function use(value: unknown) {}
+      function run(): [unknown, unknown] {
+        const [fnsWire, connect] = createFnsWire({});
+        connect();
+        const obj = {};
+        fnsWire.fn('test', () => {
+          use(obj);
+        });
+        return [fnsWire, obj];
+      }
+
+      let value = run() as any;
+      let [wire, obj] = value;
+      value = null;
+      const detectorWire = new LeakDetector(wire);
+      const detectorObj = new LeakDetector(obj);
+      obj = null;
+      expect(await detectorObj.isLeaking()).toBe(true);
+      wire = null;
+      expect(await detectorWire.isLeaking()).toBe(false);
+      expect(await detectorObj.isLeaking()).toBe(false);
+    });
+    it('should not leak wire after connect and subscribe', async () => {
+      function run(): [unknown, unknown] {
+        const [wire1, connect1] = createFnsWire({});
+        const [wire2, connect2] = createFnsWire(wire1);
+        connect1();
+        connect2();
+        wire2.fn('test', () => {});
+        return [wire1, wire2];
+      }
+
+      let values = run() as any;
+      let [wire1, wire2] = values;
+      values = null;
+      const detector1 = new LeakDetector(wire1);
+      const detector2 = new LeakDetector(wire2);
+      wire1 = null;
+      expect(await detector1.isLeaking()).toBe(true);
+      expect(await detector2.isLeaking()).toBe(true);
+      wire2 = null;
+      expect(await detector1.isLeaking()).toBe(false);
+      expect(await detector2.isLeaking()).toBe(false);
     });
   });
 });
